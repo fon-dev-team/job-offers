e04c97bc846502de6f6038e484f0e287d0d700e7
Álvaro Aroca <alvaro.aroca.m@gmail.com>
2016-02-16 18:04:41 +0100

    Formatting code

--- com/fon/jobs/applicant/AlvaroAroca.java
+++ com/fon/jobs/applicant/AlvaroAroca.java
@@ -5,24 +5,24 @@ import com.fon.jobs.JavaDeveloper;
 public class AlvaroAroca extends JavaDeveloper {
     public String name = "Álvaro";
     public String surname = "Aroca Muñoz";
-    
+
     public String address = "Mairena del Aljarafe, Sevilla";
 
-	private String email = "alvaro.aroca.m@gmail.com";
-    
+    private String email = "alvaro.aroca.m@gmail.com";
+
     @Override
-	public List<String> getActualTechnologies() {
-		List<String> techs = new ArrayList<>();
-		techs.add("Java 7-8");
-		techs.add("Tomcat");
-		techs.add("Spring MVC");
+    public List<String> getActualTechnologies() {
+        List<String> techs = new ArrayList<>();
+        techs.add("Java 7-8");
+        techs.add("Tomcat");
+        techs.add("Spring MVC");
         techs.add("Spring Spring");
         techs.add("Struts 2");
-		techs.add("Sql   - Mysql");
-		techs.add("Git");
-		techs.add("Eclipse");
-		techs.add("Maven");
-		techs.add("Continuous Integration - Jenkins, Sonarqube");
+        techs.add("Sql   - Mysql");
+        techs.add("Git");
+        techs.add("Eclipse");
+        techs.add("Maven");
+        techs.add("Continuous Integration - Jenkins, Sonarqube");
         techs.add("jQuery");
         techs.add("Bootstrap");
         techs.add("Foundation");
@@ -30,25 +30,25 @@ public class AlvaroAroca extends JavaDeveloper {
         techs.add("Cordova");
         techs.add("AWS");
 
-		return techs;
-	}
-    
-    public List<String> getInProgressTechnologies() {
-        List<String> techs = new ArrayList<>();
-        
-        techs.add("Node.js");
-        techs.add("Javascript");
-        
         return techs;
     }
-    
+
+    public List<String> getInProgressTechnologies() {
+        List<String> techs = new ArrayList<>();
+
+        techs.add("Node.js");
+        techs.add("Javascript");
+
+        return techs;
+    }
+
     protected List<String> getSocial() {
         List<String> social = new ArrayList<>();
-        
+
         social.add("https://www.linkedin.com/in/aaroca");
         social.add("https://github.com/aaroca");
         social.add("https://twitter.com/aarocatwitt");
-        
+
         return social;
     }
 }
